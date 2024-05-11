607a646ee227e80d6a006a1c9fa51abe1947a5da
MaksimTikhonenko <danchukmaks1736@gmail.com>
2024-05-12 01:06:08 +0300

    АнаÐліз_даних

--- Аналіз_даних.py
+++ Аналіз_даних.py
@@ -1,31 +1,31 @@
 # Задача: Аналіз даних про книги
 
+# Вхідні дані:
 list_dct = [{'author': 'Tolkien', 'book': 'The Hobbit', 'pages_count': 310},
             {'author': 'Orwell', 'book': '1984', 'pages_count': 328},
             {'author': 'Tolkien', 'book': 'The Lord of the Rings', 'pages_count': 1178},
-            {'author': 'Orwell', 'book': 'Animal Farm', 'pages_count': 112}] # Список словників, кожен словник містить 3 ключа і 3 значення
+            {'author': 'Orwell', 'book': 'Animal Farm', 'pages_count': 112}]
+# Вихідні дані:
 result = {} # Словник що містить ключ = 1 значення (ім'я автора) : кортеж (2 значення, 3 значення)
 
-a = list_dct[0]  # Розбиваю список на різні словники, щоб працювати з кожним словником окремо
-b = list_dct[1]
-c = list_dct[2]
-d = list_dct[3]
+for author_book in list_dct:
+    author = author_book['author']
+    book = author_book['book']
+    pagescount = author_book['pages_count']
 
-#TODO
+    m = result.get(author)
 
-for i in a.items():
+    # визначити чи ця книга є книгою з максимальною кільк. сторінок у цього автора
 
-    print(i)
+    # якщо у нас вже є найбільша книга для цього і вона має більше сторінок - переходимо на наступну книгу з списку
+    if m and pagescount < m[1]:
+        continue
 
-for k, v in a:
-    if k == 'author':
-        new_a = {v: ()}
-        if k == 'book':
-            new_a[v] = v
-        if k == 'pages_count':
-            new_a[v] = v
-print(new_a)
+    # якщо попередньої найбільшої книжки ще не існує для цього автора
+    # або
+    # попередня найбільша книжка має менше сторінок ніж та на яку ми дивимся в циклі зараз
+    # то треба зберегти ту на яку дивимся зараз як найбільшу для цього автора
+    result[author] = (book, pagescount)
 
+print(result)
 
-
-#result = {...} #todo Перебираю список словнік, витягую лише ті словники у яких максимальне значення сторінок у книзі
--- Злиття_списків.py
+++ Злиття_списків.py
@@ -1,5 +1,5 @@
 num = [("integer", 5), ("float", 3.2), ("integer", 15), ("float", 7.5), ("integer", 10), ("float", 3.5),
-       ("integer", 8), ("float", 7.9), ("integer", 3), ("float", 9.1), ("integer", 20), ("float", 4.9)]
+       ("integer", 8), ("float", 7.9), ("integer", 3), ("float", 9.1), ("integer", 20), ("float", 4.9), ("float", 4.9)]
 sort_num = {}
 
 
@@ -13,7 +13,7 @@ sort_num = {}
 num_k = {x[0] for x in num}
 
 for k in num_k:
-       v = {y[1]for y in num if y[0] == k}
+       v = [y[1]for y in num if y[0] == k]
        sort_num[k] = sum(v)
 
 print(sort_num)

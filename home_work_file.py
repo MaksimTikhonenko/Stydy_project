e99eca1ddc03fa4f7b523aef2256373593c0be7e
MaksimTikhonenko <danchukmaks1736@gmail.com>
2024-05-25 17:17:21 +0300

    workinÐg_whis_a_file.txt

--- home_work_file.py
+++ home_work_file.py
@@ -1,2 +1,19 @@
-import random
-lst = [random]
+# Перевірка формату числа
+fl = []
+with open('input.txt', 'r') as file:  # Відкрити текстовий файл, та перевірити кожен рядок чи є він числом з плаваючою комою
+    lines = file.readlines()
+    # while inp:
+    #     inp.strip()
+    #     inp = file.readline()
+for line in lines:
+    new_line = line.strip()
+    #if isinstance(new_line, float)
+    print(isinstance(new_line, float))
+        #fl.append(new_line)
+    #else:
+        #continue
+
+
+#print(fl)
+
+
